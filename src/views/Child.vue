ea1bd6ae38dac9b306465cb0182ff09138368e1a
Olena Rubanenko <alenkaru96@gmail.com>
2018-08-17 20:37:01 +0300

    fit george img

--- src/views/Child.vue
+++ src/views/Child.vue
@@ -7,9 +7,8 @@
 <style lang="stylus" scoped>
 .image
     position absolute
-    width 800px
-    height 400px
-    margin 10em 5em 0 15em
+    width 450px
+    height 245px
     background-image: url('../assets/ISB_Georg.png')
     background-size cover
     background-repeat no-repeat
